13b13b65ac676e5358db9cecb07db9aec3d7fe49
Bladi Duran <bladi.duran@gmail.com>
2021-03-15 15:27:32 -0400

    mzii

--- server/controllers/BlogController.js
+++ server/controllers/BlogController.js
@@ -1,31 +1,8 @@
 const express = require('express')
-const PostModel = require('../models/Post.js').model
+var BlogController = {}
 
-
-var blogController = {}
-
-blogController.index = (req, res) => {
+BlogController.index = (req, res) => {
     res.render('blog/index')
 }
 
-blogController.show = (req, res) => {
-    res.render('blog/show')
-}
-blogController.create = (req, res) => {
-    res.render('blog/create')
-}
-blogController.store= (req, res) => {
-
-    PostModel.create({title: 'My First Post', description: 'hey this is a cool post about nothing'}, function(err, data) {
-        console.log(data)
-    })
-
-
-
-    res.send(req.body)
-}
-
-
-
-
-module.exports = blogController
+module.exports = BlogController
--- server/routes/web.js
+++ server/routes/web.js
@@ -12,9 +12,6 @@ router.get('/about', pagesController.aboutPage)
 router.get('/contact', pagesController.contact)
 
 router.get('/blog', blogController.index)
-router.get('/blog/create', blogController.create)
-router.get('/blog', blogController.index)
-router.get('/blog', blogController.index)
 
 
 /**
