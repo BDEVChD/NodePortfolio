82f935fa3dbc197b2e03f9228787d307ddf53c11
Bladi Duran <bladi.duran@gmail.com>
2021-03-23 15:50:52 -0400

    a

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express()
 const pug = require('pug')
 const path = require('path')
 require('dotenv').config()
+const port = process.env.port || 3000
 
 app.use(express.static('client/public'))
 
@@ -11,7 +12,7 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, '../client/src/views'))
 
 app.use(webRoutes)
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
     console.log('Server is running on http://www.localhost:3000 if bladi wants to stop he can press clear ' + process.env.DB_USERNAME)
 })
 
